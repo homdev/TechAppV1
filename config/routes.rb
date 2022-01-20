b3a0fa020fa2fff3904af3c10b7706f8b2abdb0f
homdev <homdev44@gmail.com>
2022-01-20 14:23:24 +0100

    add devise auth company & technician

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'Company', at: 'company_auth'
+
+  mount_devise_token_auth_for 'Technician', at: 'technician_auth'
+  as :technician do
+    # Define routes for Technician within this block.
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
